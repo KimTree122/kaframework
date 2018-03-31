7ea05ac5e3d97a7c3feb7dfefd2c5ec305dae994
kimtree122 <50930985@qq.com>
2018-03-31 17:26:08 +0800

    修正二维码扫描框拉伸问题

--- kfbll/src/main/java/com/kim/kfbll/QRScan/camera/CameraConfigurationManager.java
+++ kfbll/src/main/java/com/kim/kfbll/QRScan/camera/CameraConfigurationManager.java
@@ -58,7 +58,20 @@ final class CameraConfigurationManager {
     Display display = manager.getDefaultDisplay();
     screenResolution = new Point(display.getWidth(), display.getHeight());
     Log.d(TAG, "Screen resolution: " + screenResolution);
-    cameraResolution = getCameraResolution(parameters, screenResolution);
+    /***by wrz 20170213 new****/
+    Point screenResolutionForCamera = new Point();
+    screenResolutionForCamera.x = screenResolution.x;
+    screenResolutionForCamera.y = screenResolution.y;
+    // preview size is always something like 480*320, other 320*480
+    if (screenResolution.x < screenResolution.y) {
+      screenResolutionForCamera.x = screenResolution.y;
+      screenResolutionForCamera.y = screenResolution.x;
+    }
+    cameraResolution = getCameraResolution(parameters, screenResolutionForCamera);
+    /***by wrz 20170213 new end****/
+    //    cameraResolution = getCameraResolution(parameters, screenResolution); //by wrz 修改 20170213
+
+
     Log.d(TAG, "Camera resolution: " + screenResolution);
   }
 
