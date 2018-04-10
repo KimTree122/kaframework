f7e35d101e90b6c2214434c26ef7bb7c3866dc9c
kimtree122 <839414748@qq.com>
2018-04-10 11:58:22 +0800

    测试界面增加权限判断

--- app/src/main/java/com/kim/kaframework/UIpackage/Activity/FuntionTest.java
+++ app/src/main/java/com/kim/kaframework/UIpackage/Activity/FuntionTest.java
@@ -1,11 +1,16 @@
 package com.kim.kaframework.UIpackage.Activity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringDef;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.Xml;
@@ -14,6 +19,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.kim.kaframework.MessageEvent;
 import com.kim.kaframework.sysData;
@@ -46,6 +52,8 @@ import BaseActivity.AbsBaseActivity;
 import HttpHelper.OKhttphelper;
 import okhttp3.Call;
 import okhttp3.Response;
+
+import static android.support.v4.content.PermissionChecker.PERMISSION_GRANTED;
 
 
 public class FuntionTest extends AbsBaseActivity implements View.OnClickListener {
@@ -274,7 +282,41 @@ public class FuntionTest extends AbsBaseActivity implements View.OnClickListener
 
     }
 
+    private boolean checkPermissionAllGranted(String[] permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                // 只要有一个权限没有被授予, 则直接返回 false
+                return false;
+            }
+        }
+        return true;
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            for (int i = 0; i < permissions.length; i++) {
+                if (grantResults[i] == PERMISSION_GRANTED) {
+                    Toast.makeText(this, "" + "权限" + permissions[i] + "申请成功", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "" + "权限" + permissions[i] + "申请失败", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
 
-
+    public void requestAllPower() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            }
+        }
+    }
 }
